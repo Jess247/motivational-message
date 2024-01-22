1d4cffd4cadd719f6ea9d31f462522652663b60a
Jessica <jesss8692@hotmail.com>
2024-01-22 16:07:11 +0100

    Initialize firebase

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://messgage-app-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const msgInDB = ref(database, 'messages/')
+
+const msg = {
+    form: "",
+    text: "",
+    to: ""
+}
+console.log(msgInDB)
+
+
+console.log(app)
+
 const textareaEl = document.querySelector(".textarea")
 const textfieldToEl = document.querySelector("#to-textfield")
 const textfieldFromEl =  document.querySelector("#from-textfield")
@@ -6,15 +27,22 @@ const msgContainer = document.querySelector(".messages")
 
 btn.addEventListener("click", renderMsg)
 
+function writeMsg() {
+    msg.form = textfieldFromEl.value
+    msg.to = textfieldToEl.value
+    msg.text = textareaEl.value
+    push(msgInDB, msg)
+}
 function renderMsg() {
-    let msg = `
+    let msgHTML = `
     <div class="msg">
     <p class="to">${textfieldToEl.value}</p>
     <p class="text">${textareaEl.value}</p>
     <p class="from">${textfieldFromEl.value}</p>
     </div>`
+    writeMsg()
     clearValues()
-    msgContainer.innerHTML += msg 
+    msgContainer.innerHTML += msgHTML 
 }
 
 function clearValues() {
